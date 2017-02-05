8f59121a7ec5f41d5943e8ee5d98ec7e5cb12fea
Alex Basson <alexbasson@gmail.com>
2017-02-04 23:21:12 -0500

    Point package dependencies at github instead of local file system

--- Package.swift
+++ Package.swift
@@ -3,8 +3,8 @@ import PackageDescription
 let package = Package(
     name: "SwiftDIWeb",
     dependencies: [
-        .Package(url: "../SwiftDIHLP", majorVersion: 0),
-        .Package(url: "../SwiftDIWebRepositories", majorVersion: 0),
+        .Package(url: "https://github.com/alexbasson/SwiftDIHLP.git", majorVersion: 0),
+        .Package(url: "https://github.com/alexbasson/SwiftDIWebRepositories.git", majorVersion: 0),
         .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1),
         .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1),
         .Package(url: "https://github.com/IBM-Swift/Kitura-StencilTemplateEngine.git", majorVersion: 1)
