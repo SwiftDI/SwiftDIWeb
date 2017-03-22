f6f676edca0007d7147a0ff09fe7eb7d23cafccf
Alex Basson <alexbasson@gmail.com>
2017-03-21 20:34:40 -0400

    Update Package.swift to point to new repositories

--- Package.swift
+++ Package.swift
@@ -3,8 +3,8 @@ import PackageDescription
 let package = Package(
     name: "SwiftDIWeb",
     dependencies: [
-        .Package(url: "https://github.com/alexbasson/SwiftDIHLP.git", majorVersion: 0),
-        .Package(url: "https://github.com/alexbasson/SwiftDIWebRepositories.git", majorVersion: 0),
+        .Package(url: "https://github.com/SwiftDI/SwiftDIHLP.git", majorVersion: 0),
+        .Package(url: "https://github.com/SwiftDI/SwiftDIWebRepositories.git", majorVersion: 0),
         .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1),
         .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1),
         .Package(url: "https://github.com/IBM-Swift/Kitura-StencilTemplateEngine.git", majorVersion: 1)
